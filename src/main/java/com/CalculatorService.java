278f3a353cda3ee02dcee87e3e1b22e918865d5a
Shubham Singh Kushwah <shubhamkushwah123@gmail.com>
2020-07-21 14:45:32 +0530

    junit test case

--- src/main/java/com/CalculatorService.java
+++ src/main/java/com/CalculatorService.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorService {
 	
-	@GetMapping("/hello")
-	public String sayHello() {
-		return "Hello World";
+	@GetMapping("/sum")
+	public int sum() {
+		return 10+20;
 	}
 
+	
+	@GetMapping("/multiply")
+	public int multiply() {
+		return 10*20;
+	}
 }
